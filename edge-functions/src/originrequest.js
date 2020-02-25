108bb3ba048db2376067206282a1e92abc875345
James Prendergast <jmpre@amazon.com>
2020-02-26 10:34:29 +1100

    log originrequest customContentHeader

--- edge-functions/src/originrequest.js
+++ edge-functions/src/originrequest.js
@@ -9,28 +9,22 @@ const customContentHeader = 'x-content-pool';
 exports.handler = (event, context, callback) => {
     const request = event.Records[0].cf.request;
     const headers = request.headers;
+    const requestOrigin = request.origin.s3;
+    let s3Origin = buckets['a']; 
 
     console.log("origin request");
     console.log(JSON.stringify(event));
 
     if (headers && headers[customContentHeader]) {
-
-        if (headers[customContentHeader][0].value !== '') {
-
-            let s3Origin = buckets[headers[customContentHeader][0].value]; 
-            /* Set S3 origin fields */
-            request.origin = {
-                s3: {
-                    domainName: s3Origin,
-                    region: 'ap-southeast-2',
-                    authMethod: 'none',
-                    path: '',
-                    customHeaders: {}
-                }
-            };
-            request.headers['host'] = [{ key: 'host', value: s3Origin }];
+        console.log('headers[customContentHeader] == ' + headers[customContentHeader]);
+        if (headers[customContentHeader][0].value !== ''){
+            s3Origin = buckets[headers[customContentHeader][0].value]; 
         }
     }
+
+    requestOrigin.region = 'ap-southeast-2'; 
+    requestOrigin.domainName = s3Origin;
+    request.headers['host'] = [{ key: 'host', value: s3Origin }];
 
     console.log(JSON.stringify(request));
 
