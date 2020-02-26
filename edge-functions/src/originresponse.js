5f844341e2d9c77bd208d424adca48e7ad9f9e1e
James Prendergast <jmpre@amazon.com>
2020-02-27 10:48:21 +1100

    Set pool cookie in OriginResponse to match origin

--- edge-functions/src/originresponse.js
+++ edge-functions/src/originresponse.js
@@ -5,8 +5,15 @@ exports.handler = (event, context, callback) => {
     console.log("origin response");
     console.log(JSON.stringify(event));
 
-    const response = event.Records[0].cf.response;
-    response.headers['Set-Cookie'] = [{key:'Set-Cookie', value: 'barbs=streisandh'}];
+    const response = event.Records[0].cf.response; //response from the origin
+    const reqHeaders = event.Records[0].cf.request; //request from cloudfront
+
+    let poolorigin = 'a'; //default origin pool
+
+    if(reqHeaders.origin.s3.domainName === 'ab-test-b.s3.ap-southeast-2.amazonaws.com'){
+        poolorigin = 'b';
+    }
+    response.headers['Set-Cookie'] = [{key:'Set-Cookie', value: `pool=${poolorigin}`}];
 
     console.log(JSON.stringify(response));
 
